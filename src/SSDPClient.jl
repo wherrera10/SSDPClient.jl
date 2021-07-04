06c407bbfd8268d2ae0472fa7b5b9d49a0652ec1
Bill <wherrera10@users.noreply.github.com>
2021-07-03 21:05:43 -1000

    Update SSDPClient.jl

--- src/SSDPClient.jl
+++ src/SSDPClient.jl
@@ -11,9 +11,10 @@ Query local network for devices responding to query with the
 Simple Service Discovery Protocol (SSDP).
 
 The optional argument is a text to match, such as "Roku" or "Arduino".
+The matching is case-insensitive.
 
-If you specify a capture in the match text that will be returned,
-otherwise the entry that matched will be returned in entirety.
+If you specify a capture in the match text, that capture will be returned,
+otherwise the entry that had a match will be returned in its entirety.
 
 NOTE: If you do not specify an argument all replies will be printed and
 the function will loop until interrupted. This is useful when searching a network.
