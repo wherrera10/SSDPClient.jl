19b45a90a711b4bfc1fe6a8e5496a4271cb6144e
Bill <wherrera10@users.noreply.github.com>
2021-07-03 21:34:24 -1000

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,10 @@
 using SSDPClient
 using Test
 
-str = ssdpquery("Roku", timeoutsecs = 30)
+str = ssdpquery("uuid", timeoutsecs = 15)
+@test contains(str, "USN")
 
+#str = ssdpquery("Roku", timeoutsecs = 30)
 # @test contains(str, "Roku")
 
 true
